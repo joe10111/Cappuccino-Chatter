0d577e237642d3af0f273ba19f21fe8ed1deb8aa
joe10111 <centeno.joe@gmail.com>
2023-08-08 11:00:04 -0700

    Finished Iteration 3 - Made user stats controller and veiw - Made User stat methods in model - made tests for all user stats

--- MvcMessageLogger/Models/User.cs
+++ MvcMessageLogger/Models/User.cs
@@ -18,5 +18,54 @@
             Password = password;
             CoffeeOfChoice = coffeeOfChoice;
         }
+        public string MostUsedWord(User user)
+        {
+            List<string> words = new List<string>();
+
+                foreach (var message in user.Messages)
+                {   // split each message into a list of words
+                    foreach (var word in message.Content.Split())
+                    {   // add all the words into the empty list we made above
+                        words.Add(word);
+                    }
+                }
+            
+
+            var MostCommonWords = words.GroupBy(x => x)
+                            .Select(x => new { word = x.Key, wordCount = x.Count() })
+                            .OrderByDescending(x => x.wordCount)
+                            .Take(1);
+
+
+            return MostCommonWords.First().word;
+        }
+
+        public string HourWithMostMessages(User user)
+        {
+            var mostActiveHour = user.Messages
+        // - Use LINQ methods to extract the hour component from each CreatedAt value.
+        .GroupBy(messages => messages.CreatedAt.Hour)
+        // - Use select to make an anonymous type containing hour and count of messages
+        .Select(messageHourGroup => new
+        {    // make hour feild and set it equal to messageHourGroup.key
+            hour = messageHourGroup.Key,
+            // make messageCount feild and set it equal to messageHourGroup.Count()
+            messageCount = messageHourGroup.Count()
+        }).OrderByDescending(groupedData => groupedData.messageCount)
+        .FirstOrDefault();
+
+            TimeSpan hourAsTimeSpan = TimeSpan.FromHours(mostActiveHour.hour);
+
+            string formattedStringHour = hourAsTimeSpan.ToString("hh':'mm");
+
+            return formattedStringHour;
+        }
+
+        public string CountOfCoffeeInMessages(User user)
+        {
+            var coffeeCount = user.Messages.FindAll(m => m.Content.ToLower().Contains("coffee")).Count();
+            
+            return coffeeCount.ToString();
+        }
     }
 }
